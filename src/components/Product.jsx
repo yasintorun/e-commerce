f5d042bdb8c4604675e55d34a2a970775b91883f
Yasin Torun <yasintorun4680@gmail.com>
2023-04-29 14:24:41 +0300

    add

---
+++ src/components/Product.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div className="w-full grow-0 xl:w-[30%] p-6 flex flex-col border m-2 hover:shadow-lg hover group">
+            <a href="#">
+                <img className="hover:grow" src="/kazak.jpeg" />
+                <div className="flex items-center justify-between pt-6">
+                    <div>
+                        <h3 className="font-hk text-base text-secondary">Cat eye</h3>
+                        <div className="flex items-center">
+                            <div className="flex items-center">
+                                <i className="fa-solid fa-star text-red-500"></i>
+                                <i className="fa-solid fa-star text-red-500"></i>
+                                <i className="fa-solid fa-star text-red-500"></i>
+                                <i className="fa-solid fa-star text-red-500"></i>
+                                <i className="fa-solid fa-star text-red-500"></i>
+                            </div>
+                            <p className="ml-2 font-hk text-sm text-secondary">
+                                (45)
+                            </p>
+                        </div>
+                    </div>
+                    <span className="font-hk text-xl font-bold text-primary">$75.0</span>
+                </div>
+                <button className="opacity-0 group-hover:opacity-100 text-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700">
+                    Sepete Ekle
+                </button>
+                {/* <div className="pt-3 flex items-center justify-between">
+                    <p className="">Siyah Kazak</p>
+                    <svg className="h-6 w-6 fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                    </svg>
+                </div>
+                <p className="pt-1 text-gray-900">120 TL</p> */}
+            </a>
+        </div>
+    )
+}
+
+export default Product
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
+import HomeSlider from '@/components/HomeSlider'
+import Product from '@/components/Product'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,109 +17,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <Header />
+      {/* <HomeSlider /> */}
+      <main>
+        <section class="bg-white py-8">
+
+          <div class="container mx-auto">
+            <div className='md:flex'>
+              <aside class="sticky top-0 min-w-[300px]">
+                <ul>
+                  {["Askeri Ayakkabılar", "Bröveler", "Gömlek", "İşbaşı kepi", "İşbaşı",
+                    "Kazak", "Kokart", "Rütbeler", "Şerit Rozeti", "Üniformalar"].map((item, i) => (
+                      <li className='border-b border-gray-200'>
+                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">{item}</a>
+                      </li>
+                    ))}
+                </ul>
+              </aside>
+              <div className='flex items-center flex-wrap pt-4 pb-12'>
+                {Array.from({ length: 20 }).map((item, i) => (
+                  <Product key={i} />
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </section>
       </main>
     </>
   )
